ca11cad089a9960fd259e928cdee17ca74fd1975
Iliya Zhechev <i4ko95@gmail.com>
2017-05-28 11:49:01 +0300

    Hivemon init

---
+++ app/hive-monitor.js
@@ -0,0 +1,46 @@
+const request = require('request-promise');
+
+
+module.exports = class HiveMonitor {
+    constructor(repository, frequency = 5000, logger = (_ = {})) {
+        this.repository = repository;
+        this.frequency = frequency;
+        this.intervalId = undefined;
+        this.logger = logger;
+    }
+
+    start() {
+        this.stop();
+        let devices = this.repository.getAllDevices();
+
+        this.intervalId = setInterval(() => {
+            for (let name in devices) {
+                let device = devices[name];
+                device.name = name;
+                this.collectDeviceData(device);
+            }
+        }, this.frequency);
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+    }
+
+    collectDeviceData({ url, displayName, name: deviceName }) {
+        this.logger(`Start collecting data for ${ deviceName }`);
+        request({ method: 'GET', url })
+            .then(response => {
+                let sensorsData = JSON.stringify(response);
+                for (let sensorName in sensorsData) {
+                    this.repository.addSensorData(
+                        deviceName, sensorName,
+                        sensorsData[sensorName], new Date()
+                    );
+                }
+                this.logger(`Data for ${ deviceName } successfully collected`);
+            })
+            .catch(error => {
+                this.logger(`Error in connecting to device ${ deviceName } (${ error })`);
+            });
+    }
+}
---
+++ app/init-monitor.js
@@ -0,0 +1,26 @@
+let HiveRepository = require('./hive-repository');
+let HiveMonitor = require('./hive-monitor');
+
+
+let repository = new HiveRepository();
+let monitor = new HiveMonitor(repository, 5000, console.log);
+
+repository
+    .setSensor(
+        { name: 'hiveTemperature', displayName: 'Hive temperature' },
+        { name: 'piloTemperature', displayName: 'Pilo temperature' },
+        { name: 'light', displayName: 'Light' },
+        { name: 'humidity', displayName: 'Humidity' }
+    )
+    .setSensorConfig({
+        name: 'generalConfig',
+        sensorNames: ['hiveTemperature', 'piloTemperature', 'light', 'humidity' ]
+    })
+    .setDevice(
+        'generalConfig',
+        { name: 'hive0', url: '0.0.0.0', location: 'Sofia', displayName: 'Hive 0' },
+        { name: 'hive1', url: '0.0.0.1', location: 'Sofia', displayName: 'Hive 1' }
+    );
+
+console.log(`Monitoring initiated...`);
+monitor.start();
